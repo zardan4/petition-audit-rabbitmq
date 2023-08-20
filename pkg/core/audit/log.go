4491431cf9181cf7e33035201415bf0ffef30234
zardan <oleksandr.kravch@gmail.com>
2023-08-20 21:47:10 +0300

    added items list

--- pkg/core/audit/log.go
+++ pkg/core/audit/log.go
@@ -4,6 +4,20 @@ import (
 	"time"
 )
 
+const (
+	ENTITY_USER      = "USER"
+	ENTITY_PETITION  = "PETITION"
+	ENTITY_SIGNATURE = "SIGNATURE"
+
+	ACTION_CREATE   = "CREATE"
+	ACTION_UPDATE   = "UPDATE"
+	ACTION_GET      = "GET"
+	ACTION_DELETE   = "DELETE"
+	ACTION_REGISTER = "REGISTER"
+	ACTION_LOGIN    = "LOGIN"
+	ACTION_LOGOUT   = "LOGOUT"
+)
+
 type LogItem struct {
 	Entity    string    `json:"entity" bson:"entity"`
 	Action    string    `json:"action" bson:"action"`
